fa246c5630aeaef173eceee53872cfc5af0e0bc9
Zain Ul Hassan <zain.hassan1212@gmail.com>
2019-09-19 21:28:53 +0500

    Update detail page

--- src/pages/ProductDetail/ProductDetail.js
+++ src/pages/ProductDetail/ProductDetail.js
@@ -2,11 +2,12 @@ import React from "react";
 
 import "./ProductDetail.css";
 
-const data = 'Resplendent in metal and glass, it’s thoughtfully designed to fit comfortably in your hands. A work of art, you won’t want to put down.Lose yourself to an expansive view on the Galaxy A8+ with the Infinity Display. Its 15.36 cm (6.0”) large display makes any view, simply captivating. And the 18.5 : 9 screen ratio provides a stunning cinematic experience that’s truly immersive.It’s time to turn pro with the Galaxy A8+. Its rear 16 MP camera takes stunning pictures that are bright and clear. The dual front camera 16 MP FF (F1.9) + 8 MP (F1.9) with Live Focus mode takes selfies & wefies to another level. Blur the background, add bokeh effect to make you and your friend stand out and if that’s not enough, readjust the effect later on.';
+const data =
+  "Resplendent in metal and glass, it’s thoughtfully designed to fit comfortably in your hands. A work of art, you won’t want to put down.Lose yourself to an expansive view on the Galaxy A8+ with the Infinity Display. Its 15.36 cm (6.0”) large display makes any view, simply captivating. And the 18.5 : 9 screen ratio provides a stunning cinematic experience that’s truly immersive.It’s time to turn pro with the Galaxy A8+. Its rear 16 MP camera takes stunning pictures that are bright and clear. The dual front camera 16 MP FF (F1.9) + 8 MP (F1.9) with Live Focus mode takes selfies & wefies to another level. Blur the background, add bokeh effect to make you and your friend stand out and if that’s not enough, readjust the effect later on.";
 const productDetail = ({ location }) => {
   const { product } = location.state;
 
-  console.log('data here', product);
+  console.log("data here", product);
 
   return (
     <div className="container product-detail-container">
@@ -18,28 +19,50 @@ const productDetail = ({ location }) => {
             alt={product.name}
           />
         </div>
-        <div className="col-12 col-md-6 mt-5 mb-5">
-          <h2>{product.name}</h2>
-          <p>{product.description}</p>
+        <div className="col-12 col-md-6 mt-2 ">
+          <h6 className="text-uppercase">
+            <strong>{product.name}</strong>
+          </h6>
+          <p className="description">
+            Resplendent in metal and glass, it’s thoughtfully designed to fit
+            comfortably in your hands. A work of art, you won’t want to put
+            down.Lose yourself to an expansive view on the Galaxy A8+ with the
+            Infinity Display. Its 15.36 cm (6.0”) large display makes any view,
+            simply captivating. And the 18.5 : 9 screen ratio provides a
+            stunning cinematic experience that’s truly immersive.It’s time to
+            turn pro with the Galaxy A8+. Its rear 16 MP camera takes stunning
+            pictures that are bright and clear.The dual front camera 16 MP FF
+            (F1.9) + 8 MP (F1.9) with Live Focus mode takes selfies & wefies to
+            another level.
+          </p>
           <hr />
-          <h3>
-            <strong> Rs {product.sale_price}</strong>
-            <p>You save Rs. 3000 (10% off)</p>
-            <p>Local taxes included (whenever applicable)</p>
-          </h3>
+          <h6>
+            <strong> Rs {product.sale_price}</strong>&nbsp;&nbsp;&nbsp;
+            <strike>
+              <span>RS {product.mark_price}</span>
+            </strike>
+            <p>
+              <small className="sales-msg">
+                You save RS {product.mark_price} ({product.sale_msg})
+              </small>
+            </p>
+            <small>Local taxes included (whenever applicable)</small>
+          </h6>
           <hr />
-          <strong>STORAGE</strong>
-          <span> 
-          <button className="btn btn-primary">1.5 GB</button>
-          <button className="btn btn-primary">2 GB</button>
+          <small className="font-weight-bold">STORAGE</small>
+          <span>
+            <button className="btn btn-primary m-3">1.5 GB</button>
+            <button className="btn btn-primary m-3">2 GB</button>
           </span>
-          <hr />
-          <strong>STORAGE</strong>
-          <span> 
-          <button className="btn btn-primary">BLACK</button>
-          <button className="btn btn-primary">GOLD</button>
+          <br />
+          <small className="font-weight-bold">COLORS</small>
+          <span>
+            <button className="btn btn-primary m-3">BLACK</button>
+            <button className="btn btn-primary m-3">GOLD</button>
           </span>
-          <button className="btn btn-primary">Add to Cart</button>
+          <br />
+          <hr />
+          <button className="btn btn-primary ">Add to Cart</button>
         </div>
       </div>
     </div>
--- src/sagas/productSagas.js
+++ src/sagas/productSagas.js
@@ -1,34 +1,38 @@
-import { call, takeLatest, put } from 'redux-saga/effects';
-import { GET_PRODUCTS, getProductsRequest, getProductsSuccess, getProductsFailure } from '../actions/productAction';
-import HttpHelper from '../utils/httpHelper';
+import { call, takeLatest, put } from "redux-saga/effects";
+import {
+  GET_PRODUCTS,
+  getProductsRequest,
+  getProductsSuccess,
+  getProductsFailure
+} from "../actions/productAction";
+import HttpHelper from "../utils/httpHelper";
 
 const { getRequest } = new HttpHelper();
 
 function* getProduct(action) {
+  try {
+    const payloadData = {
+      url: "https://assignment-appstreet.herokuapp.com/api/v1/products?page=1"
+    };
 
-try {
-  const payloadData = {
-    url: 'https://assignment-appstreet.herokuapp.com/api/v1/products?page=1',
-  };
+    yield put(getProductsRequest());
 
-  yield put(getProductsRequest());
+    const { data, status } = yield call(getRequest, payloadData);
 
-  const { data, status } = yield call(getRequest, payloadData);
+    if (status === 200) {
+      const { products } = data;
 
-  if(status === 200) {
-    const { products } = data;
-
-    yield put(getProductsSuccess(products));
-  } else {
+      yield put(getProductsSuccess(products));
+    } else {
+      yield put(getProductsFailure());
+    }
+  } catch (error) {
     yield put(getProductsFailure());
   }
-} catch(error) {
-  yield put(getProductsFailure());
-}
 }
 
 function* productSagas() {
   yield [takeLatest(GET_PRODUCTS, getProduct)];
-};
+}
 
 export default productSagas;
